8d503e0305229543a6b3903a3dce657f5392de53
ugadimas25 <ugadimas@gmail.com>
2026-01-08 08:24:24 +0700

    Add self-destructing service worker

---
+++ client/public/sw.js
@@ -0,0 +1,12 @@
+// Self-destructing service worker - unregisters itself immediately
+self.addEventListener('install', function() {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function() {
+  self.registration.unregister().then(function() {
+    return self.clients.matchAll();
+  }).then(function(clients) {
+    clients.forEach(client => client.navigate(client.url));
+  });
+});
